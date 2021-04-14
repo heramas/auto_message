b080042b0e9e13e218a7b92c15cc9640c11d1f22
ramas <ga11106@naver.com>
2021-04-14 12:02:47 +0900

    test

---
+++ src/test/java/com/test/source/TestSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
