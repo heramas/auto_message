89f5eb6cdd0b06ec05d270bcd743b3733af393b6
ramas <ga11106@naver.com>
2021-04-15 10:26:37 +0900

    commit

---
+++ src/main/java/com/auto/message/AutoMessageApplication.java
@@ -0,0 +1,13 @@
+package com.auto.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auto/message/AutoMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auto.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
