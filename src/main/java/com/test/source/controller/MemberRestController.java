d7645edb8041a3ee90b04ef9a0771005f6965bcd
ramas <ga11106@naver.com>
2021-04-15 09:54:44 +0900

    UPDATE

--- src/main/java/com/test/source/controller/MemberRestController.java
+++ src/main/java/com/test/source/controller/MemberRestController.java
@@ -1,6 +1,13 @@
 package com.test.source.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MemberRestController {
 
-	
+	@RequestMapping()
+	public String main() {
+		return "home";
+	}
 }
