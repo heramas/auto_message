37da7042c1949b537eb1b814872b620430c79605
ramas <ga11106@naver.com>
2021-04-16 18:00:11 +0900

    add filter run

--- src/main/java/com/auto/message/AutoMessageApplication.java
+++ src/main/java/com/auto/message/AutoMessageApplication.java
@@ -2,8 +2,10 @@ package com.auto.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class AutoMessageApplication {
 
 	public static void main(String[] args) {
